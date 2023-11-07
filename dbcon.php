072a307f03c452eb65511f30d85ca442230f067a
Riadh Hannachi <58027951+hriadh@users.noreply.github.com>
2023-11-07 21:07:31 +0100

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("interappp-server","fyoesptqtq","XP2A26144L4BA53L$","intermediationphp");
+$con = mysqli_connect("interappp-server.mysql.database.azure.com","fyoesptqtq","XP2A26144L4BA53L$","intermediationphp");
 
 if(!$con){
     die('Connexion échouée'. mysqli_connect_error());
